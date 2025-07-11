d4ef9ca4eab1453f19ffaae19959ddf5399dec5f
katomana-desu <katomanamanamana@gmail.com>
2025-07-11 20:01:09 +0900

    week9の途中までできたよん問題2まで

---
+++ ph-1 karikyuramu/js/script.js
@@ -0,0 +1,12 @@
+console.log("こんにちは")
+/*let hello = こんにちは*/
+console.log("I am learning JavaScript")
+console.log(5*7)
+console.log("2+5")
+console.log(9%4)
+console.log(3**4)
+console.log(4*8/2+4-10)
+console.log(4*40/2-65+5)
+console.log(8+2-5*12/2+50)
+let variable = "POSSE"
+
